c002df7378887a15908ee2e15bed68980564da0f
Trilok Behere <trilok.behere@gmail.com>
2017-08-02 15:13:56 -0400

    no message

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter = 0;
+    @IBOutlet weak var thelabel: UILabel!
+    @IBAction func Clickmebutton(_ sender: Any) {
+        thelabel.text = "button pushed"
+        print("button pushed")
+        counter += 1
+        print(counter)
+        if counter >= 10 {
+            thelabel.text = "Pushed 10 times"
+        }
+        
+    }
+    @IBAction func buttonexe(_ sender: Any) {
+        thelabel.text = "button is cool"
+    }
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGray
+        thelabel.text = "Hello from swift"
     }
 
     override func didReceiveMemoryWarning() {
